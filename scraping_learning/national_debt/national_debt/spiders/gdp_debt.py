bef90603b2f4f4d6ed2c427d4f3b5e87bc1dcf71
Pierrorobis <pierre.robisson@gmail.com>
2020-04-14 14:14:53 +0200

    Finish debt exercises

--- scraping_learning/national_debt/national_debt/spiders/gdp_debt.py
+++ scraping_learning/national_debt/national_debt/spiders/gdp_debt.py
@@ -4,8 +4,16 @@ import scrapy
 
 class GdpDebtSpider(scrapy.Spider):
     name = 'gdp_debt'
-    allowed_domains = ['https://worldpopulationreview.com/countries/countries-by-national-debt/']
-    start_urls = ['http://https://worldpopulationreview.com/countries/countries-by-national-debt//']
+    allowed_domains = ['www.worldpopulationreview.com']
+    start_urls = ['https://worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        countries = response.xpath(
+            "//table[@class='datatableStyles__StyledTable-bwtkle-1 hOnuWY table table-striped']/tbody/tr")
+        for country in countries:
+            name = country.xpath("./td[1]/a/text()").get()
+            gdp_debt = country.xpath("./td[2]/text()").get()
+            yield {
+                "country": name,
+                "gdp_debt": gdp_debt
+            }
