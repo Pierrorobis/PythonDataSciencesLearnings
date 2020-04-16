63d0311edd46de5bb19e0b2031225e544f605378
Pierrorobis <pierre.robisson@gmail.com>
2020-04-16 15:51:42 +0200

    Add lua function containing javascript function to click.
    Javascript is mandatory here as splash uses css selectors, and these selectors
    do not allow selection based on content. This Lua & javascript allows us to do so.

--- scraping_learning/quotes/quotes/spiders/quote.py
+++ scraping_learning/quotes/quotes/spiders/quote.py
@@ -10,6 +10,46 @@ class QuoteSpider(scrapy.Spider):
     allowed_domains = ['quotes.toscrape.com'] # VERY VERY IMPORTANT, OTHERWISE IT WON'T WORK FOR THE NEXT PAGES
     start_urls = ['http://quotes.toscrape.com/js/']
 
+    # The following script is not used here, but it shows how to scroll, and click a button using javascript
+    # based on the button content !
+    script = """
+    function main(splash, args)
+      splash:set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15")
+      assert(splash:go(args.url))
+      assert(splash:wait(1))
+      
+      splash.scroll_position = {y=1800}
+      splash:wait(1)
+      
+      local get_dimensions = splash:jsfunc([[
+        function () {
+          var aTags = document.getElementsByTagName("a");
+          var searchText = "Next";
+          var found;
+    
+          for (var i = 0; i < aTags.length; i++) {
+            if (aTags[i].textContent.includes(searchText)) {
+              found = aTags[i];
+            break;
+                }
+          }
+        // Handle case with empty found var
+            var rect = found.getClientRects()[0];
+          return {"x": rect.left, "y": rect.top}
+        }
+        ]])
+      
+      local dimensions = get_dimensions()
+      
+      splash:mouse_click(dimensions.x+10, dimensions.y+10)
+      -- Wait split second to allow event to propagate.
+      splash:wait(1)
+      return {
+        dim = dimensions,
+        html = splash:png()
+        }
+    end
+    """
     def start_requests(self):
         for url in self.start_urls:
             yield SplashRequest(url, self.parse_item, args={'wait': 0.5})
